f7b67c53efc99c88debe9ee4bb83564e514a19fb
sunsuking <sunsuking@gmail.com>
2024-09-05 09:05:30 +0900

    유저 서비스 엔드포인트 수정

--- user-service/src/main/java/com/ttalkak/user/user/adapter/in/web/UserController.java
+++ user-service/src/main/java/com/ttalkak/user/user/adapter/in/web/UserController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @WebAdapter
 @RequiredArgsConstructor
-@RequestMapping("/user")
+@RequestMapping("/v1/user")
 public class UserController {
     private final FindUserUseCase findUserUseCase;
 
