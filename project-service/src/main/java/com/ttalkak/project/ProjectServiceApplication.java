9687046d1099c9102fb3129c02f14f9f0424f27f
JuneYub <ajsjdlwj0123@gmail.com>
2024-09-05 11:20:49 +0900

    프로젝트 설정

--- project-service/src/main/java/com/ttalkak/project/ProjectServiceApplication.java
+++ project-service/src/main/java/com/ttalkak/project/ProjectServiceApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @EnableFeignClients
+@EnableAspectJAutoProxy
 @SpringBootApplication
 public class ProjectServiceApplication {
 
---
+++ project-service/src/main/java/com/ttalkak/project/global/aop/LoggingAspect.java
@@ -0,0 +1,18 @@
+package com.ttalkak.project.global.aop;
+
+import jakarta.persistence.Column;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Slf4j
+public class LoggingAspect {
+
+//    @Around("within(*..InputPort)")
+//    public
+
+}
