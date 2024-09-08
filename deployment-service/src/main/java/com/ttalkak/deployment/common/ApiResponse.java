64b845dbc70b70a2835fb18133af2226d2720780
sgo722 <sgo722@naver.com>
2024-09-08 22:43:50 +0900

    feat : ApiResponse Deployment 적용

---
+++ deployment-service/src/main/java/com/ttalkak/deployment/common/ApiResponse.java
@@ -0,0 +1,31 @@
+package com.ttalkak.deployment.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.Map;
+
+@Data
+@AllArgsConstructor
+public class ApiResponse<T> {
+    private boolean success;
+    private String message;
+    private int status;
+    private T data;
+
+    public static <T> ApiResponse<T> success(T data) {
+        return new ApiResponse<>(true, "OK", 200, data);
+    }
+
+    public static <T> ApiResponse<T> success() {
+        return new ApiResponse<>(true, null, 200, null);
+    }
+
+    public static <T> ApiResponse<T> fail(String message, int status) {
+        return new ApiResponse<>(false, message, status, null);
+    }
+
+    public static ApiResponse<Map<String, String>> fail(String message, int status, Map<String, String> errors) {
+        return new ApiResponse<>(false, message, status, errors);
+    }
+}
