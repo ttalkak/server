aa5d4c72fe32f98cd7229f0a3ee08456bd479e6d
sgo722@naver.com <sgo722@naver.com>
2024-09-10 01:58:06 +0900

    feat : 데이터베이스 null 가능하도록 수정

--- deployment-service/src/main/java/com/ttalkak/deployment/deployment/framework/web/request/DatabaseCreateRequest.java
+++ deployment-service/src/main/java/com/ttalkak/deployment/deployment/framework/web/request/DatabaseCreateRequest.java
@@ -9,13 +9,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class DatabaseCreateRequest {
 
-    @NotNull(message = "데이터베이스 이름을 필수입니다.")
     private String databaseName;
 
-    @NotNull(message = "10000~20000의 포트넘버를 입력해주세요.")
     private int databasePort;
 
-    @NotNull(message = "유저 이름은 필수입니다.")
     private String username;
 
     private String password;
