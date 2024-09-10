3872bd09de2d02948a612bcf0d2fff4fac8e4d2d
sunsuking <sunsuking@gmail.com>
2024-09-10 14:32:05 +0900

    게이트웨이 수정

--- gateway-service/src/main/java/com/ttalkak/gateway/config/CorsConfig.java
+++ gateway-service/src/main/java/com/ttalkak/gateway/config/CorsConfig.java
@@ -24,8 +24,8 @@ public class CorsConfig {
         configuration.addAllowedOrigin("http://localhost:5173");
         configuration.addAllowedOrigin("http://127.0.0.1:5500");
 
-        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
-        configuration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type", "refreshToken"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH"));
+        configuration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type", "refreshToken", "Set-Cookie"));
         configuration.setAllowCredentials(true);
         configuration.setMaxAge(3600L);
 
