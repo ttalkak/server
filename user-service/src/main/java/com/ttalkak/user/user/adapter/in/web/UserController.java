fe3e0040563726f1af4b8d1371b7aaf11ffe2c8d
sunsuking <sunsuking@gmail.com>
2024-09-10 09:33:04 +0900

    유저 서비스 불필요한 임포트 수정

--- user-service/src/main/java/com/ttalkak/user/user/adapter/in/web/UserController.java
+++ user-service/src/main/java/com/ttalkak/user/user/adapter/in/web/UserController.java
@@ -1,13 +1,9 @@
 package com.ttalkak.user.user.adapter.in.web;
 
-import com.ttalkak.user.common.UseCase;
 import com.ttalkak.user.common.WebAdapter;
 import com.ttalkak.user.user.adapter.in.web.response.ApiResponse;
 import com.ttalkak.user.user.application.port.in.FindUserUseCase;
 import com.ttalkak.user.user.domain.LoginUser;
-import com.ttalkak.user.user.domain.User;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.ws.rs.HeaderParam;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
