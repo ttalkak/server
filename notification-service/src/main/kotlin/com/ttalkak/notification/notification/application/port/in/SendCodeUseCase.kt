bda47b6b9fb72546c5b4a149913e96a87b89c47e
sunsuking <sunsuking@gmail.com>
2024-09-19 11:01:11 +0900

    이메일 템플릿 변경

--- notification-service/src/main/kotlin/com/ttalkak/notification/notification/application/port/in/SendCodeUseCase.kt
+++ notification-service/src/main/kotlin/com/ttalkak/notification/notification/application/port/in/SendCodeUseCase.kt
@@ -5,6 +5,7 @@ interface SendCodeUseCase {
      * 이메일로 인증 코드를 전송한다.
      *
      * @param email 이메일
+     * @param nickname 닉네임
      */
-    fun sendCode(email: String)
+    fun sendCode(email: String, nickname: String)
 }
