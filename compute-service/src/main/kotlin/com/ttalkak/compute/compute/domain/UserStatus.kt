439fbbfc791dd71ad17ef58c2c259bf85c484502
sunsuking <sunsuking@gmail.com>
2024-09-27 10:00:51 +0900

    status 코드 수정

--- compute-service/src/main/kotlin/com/ttalkak/compute/compute/domain/UserStatus.kt
+++ compute-service/src/main/kotlin/com/ttalkak/compute/compute/domain/UserStatus.kt
@@ -3,6 +3,8 @@ package com.ttalkak.compute.compute.domain
 data class UserStatus(
     val userId: Long,
     val maxCompute: Int,
+    val maxMemory: Int,
+    val maxCPU: Double,
     val availablePortStart: Int,
     val availablePortEnd: Int
 )
