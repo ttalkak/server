ec537dc10ecc4580a14f7ff0e12777b8f384ad31
sunsuking <sunsuking@gmail.com>
2024-10-02 10:20:16 +0900

    삭제 로직 수정

--- compute-service/src/main/kotlin/com/ttalkak/compute/compute/adapter/out/cache/repository/DeploymentCacheRepository.kt
+++ compute-service/src/main/kotlin/com/ttalkak/compute/compute/adapter/out/cache/repository/DeploymentCacheRepository.kt
@@ -2,18 +2,21 @@ package com.ttalkak.compute.compute.adapter.out.cache.repository
 
 import com.ttalkak.compute.common.util.Json
 import com.ttalkak.compute.compute.adapter.out.cache.entity.DeploymentStatusCache
+import io.github.oshai.kotlinlogging.KotlinLogging
 import jakarta.annotation.Resource
 import org.springframework.data.redis.core.HashOperations
 import org.springframework.stereotype.Repository
 
 @Repository
 class DeploymentCacheRepository {
-    @Resource(name = "redisTemplate")
-    private lateinit var hashOperations: HashOperations<String, String, String>
-
     companion object {
         const val DEPLOYMENT_CACHE_KEY = "deploymentCache"
     }
+
+    @Resource(name = "redisTemplate")
+    private lateinit var hashOperations: HashOperations<String, String, String>
+
+    private val log = KotlinLogging.logger { }
 
     fun save(deploymentId: Long, deploymentStatus: DeploymentStatusCache) {
         val value = Json.serialize(deploymentStatus)
@@ -25,10 +28,12 @@ class DeploymentCacheRepository {
     }
 
     fun deleteByUserId(userId: Long) {
-        val keys = hashOperations.keys(DEPLOYMENT_CACHE_KEY)
-        keys.forEach {
+        hashOperations.keys(DEPLOYMENT_CACHE_KEY).forEach {
             val value = hashOperations.get(DEPLOYMENT_CACHE_KEY, it).toString()
             val deploymentStatus = Json.deserialize(value, DeploymentStatusCache::class.java)
+            log.info {
+                "RunningCache 삭제: $it, $deploymentStatus"
+            }
             if (deploymentStatus.userId == userId) {
                 hashOperations.delete(DEPLOYMENT_CACHE_KEY, it)
             }
