f651f87d934084f131abeb0af54b2076561ec0b1
sgo722@naver.com <sgo722@naver.com>
2024-10-04 13:23:06 +0900

    feat

--- deployment-service/src/main/java/com/ttalkak/deployment/deployment/framework/web/DeploymentController.java
+++ deployment-service/src/main/java/com/ttalkak/deployment/deployment/framework/web/DeploymentController.java
@@ -15,7 +15,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("datab")
+@RequestMapping("v1/deployment")
 public class DeploymentController {
 
     private final CreateDeploymentUseCase createDeploymentUsecase;
