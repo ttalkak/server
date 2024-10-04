0d0ebe4d0b1a338eb59c7b4489edfd394bcc3509
sunsuking <sunsuking@gmail.com>
2024-10-04 16:32:42 +0900

    전체 타임존 설정

--- project-service/src/main/java/com/ttalkak/project/ProjectServiceApplication.java
+++ project-service/src/main/java/com/ttalkak/project/ProjectServiceApplication.java
@@ -1,5 +1,7 @@
 package com.ttalkak.project;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -7,10 +9,16 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import jakarta.annotation.PostConstruct;
+
 @EnableFeignClients
 @EnableAspectJAutoProxy(proxyTargetClass = true)
 @SpringBootApplication
 public class ProjectServiceApplication {
+    @PostConstruct
+    void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ProjectServiceApplication.class, args);
--- user-service/src/main/java/com/ttalkak/user/UserServiceApplication.java
+++ user-service/src/main/java/com/ttalkak/user/UserServiceApplication.java
@@ -1,12 +1,21 @@
 package com.ttalkak.user;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+import jakarta.annotation.PostConstruct;
+
 @SpringBootApplication
 @EnableDiscoveryClient
 public class UserServiceApplication {
+    @PostConstruct
+    void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
+    
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
     }
