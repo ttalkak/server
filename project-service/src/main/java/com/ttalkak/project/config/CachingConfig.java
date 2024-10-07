7a8e73e8be96f8241000939f8e76d414badf3269
JuneYub <ajsjdlwj0123@gmail.com>
2024-10-07 17:56:28 +0900

    로컬 캐싱 기능 추가

---
+++ project-service/src/main/java/com/ttalkak/project/config/CachingConfig.java
@@ -0,0 +1,11 @@
+package com.ttalkak.project.config;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@EnableCaching
+@Configuration
+public class CachingConfig {
+
+}
