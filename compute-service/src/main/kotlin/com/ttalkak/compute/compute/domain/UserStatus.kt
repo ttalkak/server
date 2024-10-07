bf09523af95d6fc1fbc757d94d506a49a8173d41
sunsuking <sunsuking@gmail.com>
2024-10-07 14:38:02 +0900

    생성 요청 수정

--- compute-service/src/main/kotlin/com/ttalkak/compute/compute/domain/UserStatus.kt
+++ compute-service/src/main/kotlin/com/ttalkak/compute/compute/domain/UserStatus.kt
@@ -2,7 +2,7 @@ package com.ttalkak.compute.compute.domain
 
 data class UserStatus(
     val userId: Long,
-    val address: String,
+    val address: String?,
     val maxCompute: Int,
     val maxMemory: Int,
     val maxCPU: Double,
