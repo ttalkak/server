35b652a6f859058887e72732104f90ada4235708
sunsuking <sunsuking@gmail.com>
2024-10-08 11:57:04 +0900

    contract 코드 수정

--- contract-service/src/main.ts
+++ contract-service/src/main.ts
@@ -5,11 +5,6 @@ import { CustomExceptionFilter } from '@src/common/exception/exception.filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new CustomExceptionFilter());
-  app.enableCors({
-    origin: ["http://localhost:3000", "https://ttalkak.com", "http://localhost:5173"],
-    credentials: true,
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  });
   await app.listen(5555);
 }
 bootstrap();
